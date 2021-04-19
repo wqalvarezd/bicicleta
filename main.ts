6eb627536a54570b392df8b72c1c38aef584bc61
wqalvarezd <wqalvarezd@gmail.com>
2021-04-19 16:16:31 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,86 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # . . . .
+        . # . . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # # # . .
+        . # . . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # # # # .
+        . # . . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
+        `)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # # # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # # # .
+        . # # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        . . . # #
+        . . . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        . . # # #
+        . . . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        . # # # #
+        . . . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        `)
+    basic.clearScreen()
+})
